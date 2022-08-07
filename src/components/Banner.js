379ef6c252e0212ba3012aba6905223e9631c61b
Nidhi <nidhipatel439@gmail.com>
2022-08-07 17:43:58 -0400

    Change content

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -8,21 +8,21 @@ function Banner() {
         <Container fluid className='py-5'>
             <Row className='align-items-center py-5 text-center'>
                 <Col>
-                    <h2 className='text-primary px-5 pb-4'>Find the best activity related to your profile</h2>
+                    <h2 className='text-primary px-5 pb-4'>Find an activity that suits your best interest!!</h2>
                     <Button variant="primary">Explore</Button>
                 </Col>
                 <Col>
-                    <p className='m-0 px-5 fs-4'>Take the test and we will direct you to a slightly accurate group where you can create new connections in a safe and open environment</p>
+                    <p className='m-0 px-5 fs-4'>Take the test! Make new connections! Do cool things!</p>
                 </Col>
                 <Col className=''><img src={banner} alt="banner" className='img-fluid' /></Col>
             </Row>
             <Row className='align-items-center py-5 text-center'>
                 <Col className=''><img src={handshake} alt="handshake" className='img-fluid' /></Col>
                 <Col>
-                    <p className='m-0 px-5 fs-4'>Through the tests, companies will have more users that fit with the profile of the service provided</p>
+                    <p className='m-0 px-5 fs-4'>This test will direct you to opportuinities to make new connections!</p>
                 </Col>
                 <Col>
-                    <h2 className='text-primary px-5 pb-4'>Publish your class, event or course to attract more users</h2>
+                    <h2 className='text-primary px-5 pb-4'>Create a space for new users to join your community!</h2>
                     <Button variant="primary">Explore</Button>
                 </Col>
             </Row>
